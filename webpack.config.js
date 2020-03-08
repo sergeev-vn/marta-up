d207a7112c855031a78b9cc7ab7464e598534bf0
Vladimir <sergeev24085@mail.ru>
2020-03-08 11:05:18 +0700

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = (env, argv) => {
   };
 
   const files = {
-    test: /\.(png|jpe?g|gif|woff2|ttf?)$/i,
+    test: /\.(png|jpe?g|gif|woff2|ttf?|ico)$/i,
     loader: "file-loader",
     options: {
       name: "[hash].[ext]"
